5b7f6a53bb1d9538222e737c23634862abed07e8
CipherPixy032 <mollym3gtr@gmail.com>
2025-03-09 11:42:13 +0700

    Commit Praktikum 3 part 5

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use Illuminate\Support\Facades\Route;
 
-// Halaman Home
-Route::get('/', [HomeController::class, 'index'])->name('home');
-
-// Halaman Products dengan prefix /category/
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+Route::get('/', function () {
+    return view('welcome');
 });
 
-// Halaman User dengan route parameter {id} dan {name}
-Route::get('/user/{id}/{name}', [UserController::class, 'show']);
-
-// Halaman Penjualan (POS)
-Route::get('/transaction', [TransactionController::class, 'index']);
-
-// Route tambahan dari gambar
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
