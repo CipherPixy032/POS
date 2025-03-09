153383d6783c14b1c167d51cbfaaa8699688734a
CipherPixy032 <mollym3gtr@gmail.com>
2025-03-09 12:18:45 +0700

    Commit Praktikum 3 part 6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,23 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\UserModel;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
+        // tambah data user dengan Eloquent Model
         $data = [
-            'level_id' => 2,
-            'username' => 'manager_dua',
-            'nama' => 'Manager 2',
-            'password' => Hash::make('12345')
+            'nama' => 'Pelanggan Pertama',
         ];
-        UserModel::create($data);
+        UserModel::where('username', 'customer-1')->update($data); // update data user
 
-        $user = UserModel::all();
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
     }
 }
