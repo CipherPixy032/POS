8becd3916ab3e90bc6e09c61e1e1d5ac181730a8
CipherPixy032 <mollym3gtr@gmail.com>
2025-03-10 20:43:36 +0700

    Commit Praktikum 4 part 8

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserModel extends Model
 {
@@ -16,5 +17,9 @@ class UserModel extends Model
     //public $timestamps = false; // Matikan timestamps jika tidak digunakan
 
     protected $fillable = ['username', 'nama', 'password', 'level_id'];
+
+    public function level(): BelongsTo {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 
 }
