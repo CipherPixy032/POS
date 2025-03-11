03e0d4b15f98e46b115f0b399dffb6957bb8b1e2
CipherPixy032 <mollym3gtr@gmail.com>
2025-03-11 11:36:05 +0700

    Commit Praktikum 5 part 3

--- routes/web.php
+++ routes/web.php
@@ -6,23 +6,32 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    // Menampilkan halaman daftar pengguna
+    Route::get('/', [UserController::class, 'index']);
+
+    // Mengambil data pengguna dalam format JSON untuk DataTables
+    Route::post('/list', [UserController::class, 'list']);
+
+    // Menampilkan halaman form untuk membuat pengguna baru
+    Route::get('/create', [UserController::class, 'create']);
+
+    // Menyimpan data pengguna baru
+    Route::post('/', [UserController::class, 'store']);
+
+    // Menampilkan detail informasi pengguna berdasarkan ID
+    Route::get('/{id}', [UserController::class, 'show']);
+
+    // Menampilkan halaman form untuk mengedit pengguna berdasarkan ID
+    Route::get('/{id}/edit', [UserController::class, 'edit']);
+
+    // Menyimpan perubahan data pengguna berdasarkan ID
+    Route::put('/{id}', [UserController::class, 'update']);
+
+    // Menghapus data pengguna berdasarkan ID
+    Route::delete('/{id}', [UserController::class, 'destroy']);
 });
 
-
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-
-
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-Route::get('/', [WelcomeController::class, 'index']);
 
